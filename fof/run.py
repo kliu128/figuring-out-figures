c21a0d1605efc5037766289145317a7dd16bfadf
Stanley <stancao@stanford.edu>
2022-02-21 04:46:17 -0500

    implemented bleu and rouge score; something is wrong with the self.log(loss)

--- fof/run.py
+++ fof/run.py
@@ -1,4 +1,5 @@
 import argparse
+from typing import List
 
 import clip
 import torch
@@ -13,6 +14,7 @@ from torchtyping import TensorType
 from einops import rearrange
 from dataloader import ScicapDataModule
 
+from datasets import load_metric
 
 class ClipGPT2Model(pl.LightningModule):
     def __init__(self, **kwargs):
@@ -32,31 +34,41 @@ class ClipGPT2Model(pl.LightningModule):
 
         self.lr = 1e-4
 
+        self.bleu_metric = load_metric('bleu')
+        self.rouge_metric = load_metric('rouge')
+
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = parent_parser.add_argument_group("ClipGPT2Model")
         return parent_parser
 
-    def forward(self, figure, labels):
-        B = len(labels)
+    def forward(self, figure, labels: List[str]):
+        B = len(labels) 
 
+        # Encode figure using CLIP model
         image_encoding = self.image_encoder.encode_image(figure)
+        # Project CLIP 512 embedding space to GPT-2 768 embedding space
         image_conditioned: TensorType["b", 768] = F.gelu(
             self.bridge(image_encoding))
         image_conditioned = rearrange(image_conditioned, "b e -> b 1 e")
-        labels = self.tokenizer(
+        labels: TensorType['b', 't', 'e'] = self.tokenizer(
             list(labels), padding=True, truncation=True, return_tensors="pt")["input_ids"].to(self.device)
 
         dummy_token = torch.zeros(
             (B, 1), dtype=torch.int64, device=self.device)
+        # inputs_embeds.shape = [2, 24, 768]; .wte = word to embed
         inputs_embeds = torch.cat([image_conditioned,
                                    self.text_decoder.transformer.wte(labels)], dim=1)
+        # labels.shape = [2, 24]
         labels = torch.cat([dummy_token, labels], dim=1)
 
         output = self.text_decoder(
             inputs_embeds=inputs_embeds,
             labels=labels)
+        # breakpoint()
 
+        # output.logits.shape = [2, 24, 50257]
+        # can replace with output.logits, output.loss
         return output["logits"], output["loss"]
 
     def training_step(self, batch, batch_idx):
@@ -64,6 +76,36 @@ class ClipGPT2Model(pl.LightningModule):
         y_hat, loss = self(
             figure, metadata)
         return loss
+    
+    # BlEU score, ROUGE score
+
+    def validation_step(self, batch, batch_idx):
+        figure, labels = batch # input, output
+        # breakpoint()
+        # y_hat is model prediction (i.e., logits), labels is gold
+        # y_hat.shape = [2, 44, 50257]
+        logits, loss = self(figure, labels)
+        model_predictions_idxs = torch.argmax(logits, dim=-1)
+
+        # PROCESSING FOR BLEU SCORE
+        # model_predictions.shape = [2, 44]
+        tokenized_labels = [ [label.split()] for label in labels ] # for bleu_metric.compute input
+        model_predictions = [ self.tokenizer.decode(model_predictions_idxs[i]).split() 
+            for i in range(model_predictions_idxs.shape[0]) ]
+        bleu_score = self.bleu_metric.compute(predictions=model_predictions, references=tokenized_labels)
+
+        # PROCESSING FOR ROUGE SCORE
+        model_predictions = [ self.tokenizer.decode(model_predictions_idxs[i]) 
+            for i in range(model_predictions_idxs.shape[0]) ]
+        rouge_score = self.rouge_metric.compute(predictions=model_predictions, references=labels)
+
+        # LOGGING
+        # TODO: what rouge score do we want to log? Use print(self.rouge_metric)
+        # to see manual
+        self.log('validation/metrics', 
+            {'BLEU Score': bleu_score['bleu'], 'ROUGE Score': rouge_score['rouge1'].mid.fmeasure})
+        self.log('loss', loss)
+
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.lr)
