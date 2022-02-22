9412751492bfcb4281443533aa69de0057e7a6e8
Kevin Liu <kevin@kliu.io>
2022-02-22 01:13:37 -0500

    Some work on the validation step

--- fof/encdec.py
+++ fof/encdec.py
@@ -1,3 +1,4 @@
+from typing import List
 import pytorch_lightning as pl
 import transformers as tr
 import torch
@@ -13,6 +14,10 @@ class EncoderDecoderModel(pl.LightningModule):
             "gpt2", add_cross_attention=True)
 
         model = tr.VisionEncoderDecoderModel(encoder=clip, decoder=gpt2)
+        # use GPT2's eos_token as the pad as well as eos token
+        model.config.eos_token_id = model.config.decoder.eos_token_id
+        model.config.pad_token_id = model.config.eos_token_id
+
         self.model = model
         self.image_processor = tr.CLIPFeatureExtractor(
             do_resize=False,
@@ -28,13 +33,15 @@ class EncoderDecoderModel(pl.LightningModule):
         parser = parent_parser.add_argument_group("EncDecModel")
         return parent_parser
 
-    def forward(self, figure, labels):
-        B = len(labels)
-
+    def preprocess_image(self, figure):
         images = torch.split(figure.to("cpu"), split_size_or_sections=1)
         squeezed_tensors = list(map(torch.squeeze, images))
         image = self.image_processor(images=squeezed_tensors)
         image = torch.stack(image["pixel_values"])
+        return image
+
+    def forward(self, image, labels):
+        B = len(labels)
 
         text = self.text_tokenizer(
             list(labels), padding=True, truncation=True, return_tensors="pt")
@@ -46,14 +53,30 @@ class EncoderDecoderModel(pl.LightningModule):
             output_attentions=True,
         )
 
-        return output.logits, output.loss
+        # Sanity check that cross attention is working
+        assert output.cross_attentions[0].mean() != 0
+        return output
 
     def training_step(self, batch, batch_idx):
         figure, metadata = batch
-        _, loss = self(
-            figure, metadata)
-        self.log("train/loss", loss)
-        return loss
+        image = self.preprocess_image(figure)
+        output = self(
+            image, metadata)
+        self.log("train/loss", output.loss)
+        return output.loss
+
+    def validation_step(self, batch, batch_idx):
+        figure, metadata = batch
+        image = self.preprocess_image(figure).to(self.device)
+        output = self(image, metadata)
+        self.log("val/loss", output.loss)
+        # Use top-p sampling with 0.9 as the probability
+        generated = self.model.generate(
+            image, return_dict_in_generate=True, top_p=0.9)
+        decoded: List[str] = self.text_tokenizer.batch_decode(
+            generated, skip_special_tokens=True)
+
+        return output.loss
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.lr)
